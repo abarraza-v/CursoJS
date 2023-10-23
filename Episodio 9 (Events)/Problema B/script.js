1c1a68a02d2b85e29e89ff618b59084eb10061d4
Alejandro Barraza <abarraza.pk@gmail.com>
2023-10-23 13:48:38 -0300

    Problema B: avance

---
+++ Episodio 9 (Events)/Problema B/script.js
@@ -0,0 +1,50 @@
+alumnos = [{
+    nombre: "Alejandro Barraza",
+    email: "abarraza.pk@gmail.com",
+    materia: "Matemática"
+},{
+    nombre: "Sergio Diaz",
+    email: "sdiaz@gmail.com",
+    materia: "Programación"
+},{
+    nombre: "Ignacio Castillo",
+    email: "icastillo@gmail.com",
+    materia: "Cálculo 1"
+},{
+    nombre: "Matias Fernandez",
+    email: "mfernandez@gmail.com",
+    materia: "Lenguaje"
+},{
+    nombre: "Jehovanna Asuaje",
+    email: "jasuaje@gmail.com",
+    materia: "Programación"
+}];
+
+let botonConfirmar = document.querySelector(".btn-confirmar");
+
+for (let alumno in alumnos){
+    let dato = alumnos[alumno];
+    let nombre = dato["nombre"];
+    let email = dato["email"];
+    let materia = dato["materia"];
+    let htmlCode = `
+    <div class="grid-item nombre">${nombre}</div>
+    <div class="grid-item email">${email}</div>
+    <div class="grid-item materia">${materia}</div>
+    <div class="grid-item semana">
+        <select class="semana-elegida">
+            <option value="1">Semana 1</option>
+            <option value="2">Semana 2</option>
+        </select>
+    </div>`
+    document.querySelector(".grid-container").innerHTML += htmlCode;
+}
+
+botonConfirmar.addEventListener("click", ()=> {
+    let elementos = document.querySelectorAll(".semana");
+    let semanasElegidas = document.querySelectorAll(".semana-elegida")
+    for (elemento in elementos) {
+        semana = elementos[elemento];
+        semana.innerHTML = semanasElegidas[elemento].value;
+    }
+})
